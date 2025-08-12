e136f769daa92f360228572dce41b3ee334ca304
NicolasAbuin99 <nicolasabuin19@gmail.com>
2025-08-12 10:25:45 -0300

    Add files via upload

---
+++ Clase1/Clase01.java
@@ -0,0 +1,20 @@
+public class Clase01 {
+    public static void main(String[] args){
+        /*Tipos de escritura
+        camel case -> frasecamelcase (variables y metodos)
+        pascal case -> frasepascalcase (clases e interfaces)
+        snake case -> frase_snake_case (sql)
+        kebab case -> frase-kebab-case (nombre de proyectos o carpetas)
+        Repositorio:https://github.com/Francisco-Acuna/2025_2C_POO_TURNO_MANANA
+        */
+        /*Software
+          JDK 21 -> https://www.oracle.com/java/technologies/downloads/#java21
+          Extension para vsc -> extension pack for java
+          cuando aprendemos un nuevo lenguaje siempre primero comentarios
+          //TODO: indica tareas pendientes por implementar
+          //FIXME: señala errores que se deben corregir
+          //comentario javaDOC viene antes de una clase, metodo, interfaz etc
+          //instalar TODO TREE
+         */
+    }    
+}
